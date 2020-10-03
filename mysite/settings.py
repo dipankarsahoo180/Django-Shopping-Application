4e3a87870e4f4677ef7a22a7f055a7e96cd75a5b
dipankar sahoo <56668329+dipankarsahoo180@users.noreply.github.com>
2020-10-03 11:03:48 +0530

    Removed Credentials

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,8 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'managesite8@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sitemanager.18.8'
+EMAIL_HOST_USER = 'Email@gmail.com'
+EMAIL_HOST_PASSWORD = 'Password'
 EMAIL_USE_TLS = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
